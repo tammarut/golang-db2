df83982d08826fa582595a30227482711925d9da
tammarut <tammarut.nua@gmail.com>
2019-03-24 16:37:01 +0700

    add #!/bin/sh >> shellscript(init.sh)

--- _shellscript/init.sh
+++ _shellscript/init.sh
@@ -1,4 +1,5 @@
 #Set Path DB2driver
+#!/bin/sh
 export DB2HOME=/home/khunpleum/go/src/github.com/tammarut/go-sql/_clidriver/linux
 export CGO_CFLAGS=-I$DB2HOME/include
 export CGO_LDFLAGS=-L$DB2HOME/lib
